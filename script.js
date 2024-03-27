91d648e36dc75d6bb3540c61c3c03f1975c0d0c6
Marturm <86203188+Marturm@users.noreply.github.com>
2024-03-27 19:49:06 +0100

    Create playGame() function to simulate a 5 round game and console log it's the result

--- script.js
+++ script.js
@@ -29,3 +29,27 @@ function playRound(playerSelection, computerSelection) {
     else return 'Invalid player input';
 }
 
+//Write a NEW function called playGame(). Use the previous function inside of this one to play a five round game that keeps score and reports a winner or loser at the end. 
+function playGame() {
+    let playerSelection;
+    let computerSelection;
+    let playerScore = 0;
+    let computerScore = 0;
+    let roundResult;
+
+    for(let i = 0; i < 5; i++) {
+        playerSelection = prompt('Choose: Rock, Paper or Scissors!');
+        computerSelection = getComputerChoice();
+
+        roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+        if(roundResult.slice(4, 7) == 'Win') playerScore++;
+        else if(roundResult.slice(4, 8) == 'Lose') computerScore++;
+    }
+
+    if(playerScore == computerScore) return 'Tie!';
+    if(playerScore > computerScore) return 'You Win!';
+    if(playerScore < computerScore) return 'You Lose!';
+}
+
+console.log(playGame());
