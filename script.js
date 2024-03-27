68e0e6e4382351c6872f860ac694e0e351756954
Marturm <86203188+Marturm@users.noreply.github.com>
2024-03-27 19:22:36 +0100

    Create playRound() function to get a string result of around

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ function getComputerChoice() {
     else if (choice == 1) return 'Paper';
     else return 'Scissors';
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.at(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    if(playerSelection == computerSelection) return `It's a Tie!`;
+
+    if(playerSelection == 'Rock') {
+        if(computerSelection === 'Paper') return 'You Lose! Paper beats Rock';  
+    
+        else return 'You Win! Rock beats Scissors';
+    }
+    else if (playerSelection == 'Paper') {
+        if(computerSelection == 'Rock') return 'You Win! Paper beats Rock';  
+    
+        else return 'You Lose! Scissors beat Paper';
+    }
+    else if (playerSelection == 'Scissors') {
+        if(computerSelection == 'Rock') return 'You Lose! Rock beats Scissors';  
+    
+        else return 'You Win! Scissors beat Paper';
+    }
+    else return 'Invalid player input';
+}
+
