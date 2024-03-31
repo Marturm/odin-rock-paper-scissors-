4daea14d88e8e9a11b20847d3dd15385964aa781
Marturm <86203188+Marturm@users.noreply.github.com>
2024-03-31 20:11:42 +0200

    Add buttons to index.html and set eventhadler to console log both choices.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const btnRock = document.querySelector(".rock");
+const btnPaper = document.querySelector(".paper");
+const btnScissors = document.querySelector(".scissors");
+
+const textScore = document.querySelector(".score");
+
+btnRock.addEventListener("click", () => playRound("Rock", getComputerChoice()));
+btnPaper.addEventListener("click", () => playRound("Paper", getComputerChoice()));
+btnScissors.addEventListener("click", () => playRound("Scissors", getComputerChoice()));
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
 
@@ -7,49 +17,7 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.at(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-
-    if(playerSelection == computerSelection) return `It's a Tie!`;
-
-    if(playerSelection == 'Rock') {
-        if(computerSelection === 'Paper') return 'You Lose! Paper beats Rock';  
-    
-        else return 'You Win! Rock beats Scissors';
-    }
-    else if (playerSelection == 'Paper') {
-        if(computerSelection == 'Rock') return 'You Win! Paper beats Rock';  
-    
-        else return 'You Lose! Scissors beat Paper';
-    }
-    else if (playerSelection == 'Scissors') {
-        if(computerSelection == 'Rock') return 'You Lose! Rock beats Scissors';  
-    
-        else return 'You Win! Scissors beat Paper';
-    }
-    else return 'Invalid player input';
+    console.log(playerSelection);
+    console.log(computerSelection);
 }
 
-//Write a NEW function called playGame(). Use the previous function inside of this one to play a five round game that keeps score and reports a winner or loser at the end. 
-function playGame() {
-    let playerSelection;
-    let computerSelection;
-    let playerScore = 0;
-    let computerScore = 0;
-    let roundResult;
-
-    for(let i = 0; i < 5; i++) {
-        playerSelection = prompt('Choose: Rock, Paper or Scissors!');
-        computerSelection = getComputerChoice();
-
-        roundResult = playRound(playerSelection, computerSelection);
-        console.log(roundResult);
-        if(roundResult.slice(4, 7) == 'Win') playerScore++;
-        else if(roundResult.slice(4, 8) == 'Lose') computerScore++;
-    }
-
-    if(playerScore == computerScore) return 'Tie!';
-    if(playerScore > computerScore) return 'You Win!';
-    if(playerScore < computerScore) return 'You Lose!';
-}
-
-console.log(playGame());
