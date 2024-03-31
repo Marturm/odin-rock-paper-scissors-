496aacfdb3bb4915eb6fac738b95578c36a885b7
Marturm <86203188+Marturm@users.noreply.github.com>
2024-03-31 20:41:43 +0200

    Make playGame function to play a round and set game results.

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const btnRock = document.querySelector(".rock");
 const btnPaper = document.querySelector(".paper");
 const btnScissors = document.querySelector(".scissors");
 
-const textScore = document.querySelector(".score");
+const text = document.querySelector(".text");
+const score = document.querySelector(".score");
+
+let playerScore = 0;
+let computerScore = 0;
 
 btnRock.addEventListener("click", () => playRound("Rock", getComputerChoice()));
 btnPaper.addEventListener("click", () => playRound("Paper", getComputerChoice()));
@@ -16,8 +20,62 @@ function getComputerChoice() {
     else return 'Scissors';
 }
 
+function chooseWinner(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection) {return "Tie"}; 
+
+    if(playerSelection == "Rock" && computerSelection == "Paper") {return "Loose"}
+    if(playerSelection == "Rock" && computerSelection == "Scissors") {return "Win"}
+
+    if(playerSelection == "Paper" && computerSelection == "Scissors") {return "Loose"}
+    if(playerSelection == "Paper" && computerSelection == "Rock") {return "Win"}
+
+    if(playerSelection == "Scissors" && computerSelection == "Rock") {return "Loose"}
+    if(playerSelection == "Scissors" && computerSelection == "Paper") {return "Win"}
+}
+
+function displayScore() {
+    score.innerHTML = `Player score: ${playerScore} | Computer score: ${computerScore}`;
+}
+
+function displayWinner(winner) {
+    text.innerHTML = `${winner} has won!`;
+    score.innerHTML = "";
+}
+
 function playRound(playerSelection, computerSelection) {
     console.log(playerSelection);
     console.log(computerSelection);
-}
 
+    
+    
+    let msg = `You choose: ${playerSelection}, computer chooses: ${computerSelection}. `;
+
+    switch(chooseWinner(playerSelection, computerSelection)){
+        case "Tie":
+            console.log("Tie");
+            msg += "It's a Tie!";
+            break;
+        case "Loose":
+            console.log("Loose");
+            msg += "You Loose!";
+            computerScore++;
+            break;
+        case "Win":
+            console.log("Win");
+            msg += "You Win!";
+            playerScore++;
+            break;
+    }
+
+    displayScore();
+    text.innerHTML = msg;
+
+    if(playerScore == 5) {
+        displayWinner("Player");
+        [computerScore, playerScore] = [0, 0];
+    }
+    else if(computerScore == 5) {
+        displayWinner("Computer");
+        [computerScore, playerScore] = [0, 0];
+    }
+}
